1b324632ed8a01d79b163cbe9db701e13cc72663
nalinsakthivel <nalinssn@gmail.com>
2021-07-19 07:37:41 +0530

    init

--- HD/App.js
+++ HD/App.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Login from './Screens/Login';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 // import Signupheader from './Componenets/Header'
 import Signup from './Screens/Signup';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
+  const [values, setValues] = useState(false);
+  useEffect(() => {
+    getdata();
+  }, []);
+  const getdata = async () => {
+    let credentials = await AsyncStorage.getItem('credentials');
+    credentials = await JSON.parse(credentials);
+    if (credentials) {
+      setValues(true);
+    }
+  };
+
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.topconatiner}>
-      <Signup />
-      {/* <Login /> */}
-    </View>
+    <NavigationContainer>
+      {values ? (
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+    // <View style={styles.topconatiner}>
+    //   <Signup />
+    //   {/* <Login /> */}
+    // </View>
   );
 };
 
